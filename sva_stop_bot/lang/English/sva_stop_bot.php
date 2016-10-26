7297d161321f387d9c8082504a036f31bf75f314
Visman <visman2008@googlemail.com>
2016-10-26 18:50:58 +0700

    sva_stop_bot v 0.2

--- sva_stop_bot/lang/English/sva_stop_bot.php
+++ sva_stop_bot/lang/English/sva_stop_bot.php
@@ -3,10 +3,11 @@
 if (!defined('FORUM')) die();
 
 $lang_sva_stop_bot = array(
-  'Honeypot field help' => 'This field has to be empty',
-	'Error1' => 'SB: Everything broke ;)',
-	'Error2' => 'SB: Structure declaration of data is broken.',
-	'Error3' => 'SB: The transmission order of data is broken.',
-	'Error4' => 'SB: The hidden field shall be empty.',
-	'Error5' => 'SB: The browser transfers not all data.',
+	'Honeypot field help' => 'This field has to be empty',
+	'Error1' => 'StopBot: Everything broke ;)',
+	'Error2' => 'StopBot: Structure declaration of data is broken.',
+	'Error3' => 'StopBot: The transmission order of data is broken.',
+	'Error4' => 'StopBot: The hidden field shall be empty.',
+	'Error5' => 'StopBot: The browser transfers not all data.',
+	'Error6' => 'StopBot: Error of time filling this form.',
 );
--- sva_stop_bot/sva_stop_bot.inc.php
+++ sva_stop_bot/sva_stop_bot.inc.php
@@ -4,7 +4,7 @@
  * Stop Bot
  *
  * sva_stop_bot
- * Copyright (C) 2014 Visman (visman@inbox.ru)
+ * Copyright (C) 2014-2016 Visman (mio.visman@yandex.ru)
  * License http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
  */
 
@@ -17,7 +17,7 @@ class sva_sb {
 	private $empty_arr;
 	private $pas;
 	private $salt;
-	
+
 	// Class instance
 	private static $instance;
 
@@ -59,8 +59,8 @@ class sva_sb {
 
 		while (strlen($Gamma)<$Len)
 		{
-			$Seq = pack("H*",sha1($Gamma.$Seq.$this->salt));
-			$Gamma.=substr($Seq,0,8);
+			$Seq = pack("H*", sha1($Gamma.$Seq.$this->salt));
+			$Gamma .= substr($Seq,0,8);
 		}
 
 		return $String^$Gamma;
@@ -68,11 +68,11 @@ class sva_sb {
 
 
 	private function field_parser($matches) {
-		if (isset($this->fields[$matches[3]]))
-			return $matches[1].$this->fields[$matches[3]];
+		if (isset($this->fields[$matches[2]]))
+			return $matches[1].$this->fields[$matches[2]];
 
-		if (in_array($matches[3], $this->notchange_arr)) {
-			$name = $matches[3];
+		if (in_array($matches[2], $this->notchange_arr)) {
+			$name = $matches[2];
 		} else {
 			$name = substr($this->first_chr, (mt_rand() % strlen($this->first_chr)), 1);
 			$this->first_chr = str_replace($name, '', $this->first_chr);
@@ -85,24 +85,24 @@ class sva_sb {
 				$name .= substr($chars, (mt_rand() % strlen($chars)), 1);
 			}
 
-			$name = (substr($matches[3], 0, 4) == 'req_' ? 'req_' : '').$name;
+			$name = (substr($matches[2], 0, 4) == 'req_' ? 'req_' : '').$name;
 		}
 
-		$this->fields[$matches[3]] = $name;
+		$this->fields[$matches[2]] = $name;
 		return $matches[1].$name;
 	}
 
 
 	private function fields_parser($matches) {
-		$matches[0] = preg_replace_callback('%(<(input|select)[^>]*name=[\'"])([^\'"]+)%i', array($this, 'field_parser'), $matches[0]);
-		return preg_replace('%(<input[^>]*name=[\'"]form_sent[\'"][^>]*value=[\'"])([^\'"]+)%i', '$1&amp;'.base64_encode($this->encrypt(serialize($this->fields))).'&amp;',	$matches[0]);
+		$matches[0] = preg_replace_callback('%(<(?:input|select)[^>]*?name=[\'"])([^\'"]+)%i', array($this, 'field_parser'), $matches[0]);
+		return preg_replace('%(<input[^>]*?name=[\'"]form_sent[\'"][^>]*?value=[\'"])([^\'"]+)%i', '$1&lt;'.base64_encode($this->encrypt(random_key(mt_rand(6,16), true).'::'.serialize($this->fields))).'&gt;', $matches[0]);
 	}
 
 
 	public function form_parser($tpl_temp, $name) {
-		$this->fields = array();
+		$this->fields = array('sva_time' => time());
 		$this->first_chr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-		return preg_replace_callback('%<form[^>]*id=[\'"]'.$name.'[\'"].*<\/form>%ims', array($this, 'fields_parser'), $tpl_temp);
+		return preg_replace_callback('%<form[^>]*?id=[\'"]'.$name.'[\'"].*?<\/form>%ims', array($this, 'fields_parser'), $tpl_temp);
 	}
 
 
@@ -115,19 +115,27 @@ class sva_sb {
 			return false;
 		}
 
-		$s = $this->encrypt(base64_decode(str_replace('&', '', $_POST['form_sent'])));
-		if (!preg_match('%^a:\d+:{(s:\d+:"\w+";s:\d+:"\w+";|s:\d+:"\w+";i:\d+;)+}$%', $s)) {
+		$s = $this->encrypt(base64_decode(substr($_POST['form_sent'], 1, -1)));
+		if (!preg_match('%^[a-zA-Z\d]+::a:\d+:{(s:\d+:"\w+";s:\d+:"\w+";|s:\d+:"\w+";i:\d+;)+}$%', $s)) {
 			$this->form_error = 2;
 			return false;
 		}
 
-		$fields_arr = unserialize($s);
+		$s =  explode('::', $s, 2);
+		$fields_arr = unserialize($s[1]);
 		$fields_arr[] = 'endofarray';
 		reset($fields_arr);
 
 		foreach ($_POST as $key => $data) {
+			// для формы проверки токена
+			if (in_array($key, array('csrf_token', 'prev_url'))) {
+				$post[$key] = $data;
+				continue;
+			}
+
 			while ((list($key_orig, $key_post) = each($fields_arr)) && $key_post != $key) {
 			}
+
 			if (current($fields_arr) == false) {
 				$this->form_error = 3;
 				return false;
@@ -135,10 +143,10 @@ class sva_sb {
 				$post[$key_orig] = $data;
 			}
 		}
-
 		$_POST = $post;
+
 		foreach ($this->empty_arr as $key) {
-			if (!empty($_POST[$key])) {
+			if (isset($_POST[$key]) && strlen($_POST[$key])) {
 				$this->form_error = 4;
 				return false;
 			}
@@ -148,6 +156,18 @@ class sva_sb {
 			$this->form_error = 5;
 			return false;
 		}
+
+		if (!isset($fields_arr['sva_time'])) {
+			$this->form_error = 6;
+			return false;
+		}
+		$time = time() - $fields_arr['sva_time'];
+		if ($time < 3 || $time > 15 * 60) {
+			$this->form_error = 6;
+			return false;
+		}
+
+		return true;
 	}
 
 
