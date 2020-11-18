89c37b7a59601d3a5960d1ca92497f7cf4c7e018
Visman <visman2008@googlemail.com>
2020-11-18 21:56:20 +0700

    edit_period (Post editing period) v 1.0

---
+++ edit_period/lang/English.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined('FORUM')) die();
+$lang_edit_period = array(
+'Edit period'	=>	'Edit Period',
+'Duration'		=> 'Duration',
+'Duration note'	=>	'Number of Minutes  a post can be edited after posting. You can enter days by converting them into minutes.',
+'No edit'		=> 'You can\'t edit this post anymore as editing time is expired. If you need to correct anything please report to Admin and Moderators. They will take care of that.'
+);
